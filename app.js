4101ff10faca0e837833d69e3c47e69a659b37ad
sidneyCampos <sidneycampos22@gmail.com>
2020-09-14 21:47:23 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// Carregando módulos
+    const express = require("express")
+    const handlebars = require("express-handlebars")
+    const bodyParser = require("body-parser")
+    const app = express()
+    const admin = require("./routes/admin") // Para chamar o arquivo de rotas
+    const path = require("path")
+//const mongoose = require("mongoose")
+// Configurações
+    // Body Parser
+        app.use(bodyParser.urlencoded({extended: true}))
+        app.use(bodyParser.json())
+    // Handlebars
+        app.engine("handlebars", handlebars({defaultLayout: "main"}))
+        app.set("view engine", "handlebars")
+    // Mongoose
+
+    // Public
+        app.use(express.static(path.join(__dirname,"public")))
+// Rotas
+    app.use("/admin", admin)
+// Outros
+const PORT = 8081
+app.listen(PORT,() => {
+    console.log("Servidor rodando!")
+})
